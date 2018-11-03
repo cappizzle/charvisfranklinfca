8c18e5fb8082fbdd0102f3305253905ec3881e17
Scott Capps <capps.scott@gmail.com>
2018-11-03 00:16:50 -0700

    sections added

--- charvis/src/app/app.component.ts
+++ charvis/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'charvis';
+  show: boolean = false
+  toggle(){
+    this.show = !this.show
+  }
+
+  scrollToElement($element): void {
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
+    this.show = false
+  }
 }
